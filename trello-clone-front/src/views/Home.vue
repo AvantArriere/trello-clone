71b95722dd4dbace9e22ee6a1d09ddae4bf21902
kleerenn <hjlee8820@gmail.com>
2020-03-12 15:55:56 +0900

    chore: add router, vuetify

---
+++ trello-clone-front/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="main">
+  <nav class="nav-bar">
+    <img class="logo" src="../assets/trello-logo.png"/>   
+    <v-btn class="login">login </v-btn>
+    <v-btn class="signup float-right">signup </v-btn>
+  </nav>
+  <div class="body">
+  </div>
+</div>
+  
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+<style scoped>
+.main {
+  height: 100%;
+  background: linear-gradient(135deg, #0079bf, #5067c5);
+
+}
+.nav-bar {
+  height: 100px;
+  width: 100%;
+  display: flex;
+  position: relative;
+}
+.logo {
+  margin: 20px;
+  width: 200px;
+  height: 50%;
+}
+.login .signup {
+  position: absolute;
+  /* display: block; */
+  float: right;
+}
+.body {
+  width: 100%;
+  /* height: 100%; */
+}
+</style>
